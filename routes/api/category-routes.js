bfc17e09ca4429277020aea42490c528a91fc323
itsDenMat <dennismateo93@gmail.com>
2022-09-13 21:45:12 -0700

    Add route to add, update, and delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,14 +41,58 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(categoryData => {
+        if (!categoryData) {
+          res.status(404).json({ message: 'Category not available'});
+          return;
+        }
+        res.json(categoryData);
+      })
+      .catch(err => {
+        console.log(err);
+        req.status(500).json(err);
+      });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.param.id
+    }
+  })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'Delete Failed. Category not available.'});
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
 });
 
 module.exports = router;
