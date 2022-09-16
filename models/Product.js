1496b4bbb5560a1f0ab4680fe8311cbfcb900908
itsDenMat <dennismateo93@gmail.com>
2022-09-15 18:27:24 -0700

    Add columns in Product.js

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,32 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
